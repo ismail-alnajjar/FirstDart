77a7236f785ecf39aaf84c0212ee6c169b8f5b45
ismail-alnajjar <ismaillalnajjaar@gmail.com>
2025-05-31 19:14:45 +0300

    create calculate funtion +Nested if

---
+++ bin/first1.dart
@@ -0,0 +1,5 @@
+import 'package:first1/first1.dart' as first1;
+
+void main(List<String> arguments) {
+  print('Hello world: ${first1.calculate()}!');
+}
---
+++ lib/first1.dart
@@ -0,0 +1,23 @@
+double calculate({double num1 = 0, double num2 = 0, String? operation}) {
+  if (operation == '+') {
+    return num1 + num2;
+  } else if (operation == '-') {
+    return num1 - num2;
+  } else if (operation == '*') {
+    return num1 * num2;
+  } else if (operation == '/') {
+    return num2 != 0 ? num1 / num2 : double.infinity;
+  } else if (operation == '%') {
+    return num2 != 0 ? num1 % num2 : double.nan;
+  } else {
+    return num1 + num2;
+  }
+}
+
+void main() {
+  print(calculate(num1: 4, num2: 7, operation: '*'));
+  print(calculate(num1: 2, num2: 6));
+  print(calculate(num1: 100, num2: 5, operation: '/'));
+  print(calculate(num1: 85, num2: 36, operation: '-'));
+  print(calculate(num1: 24, num2: 9, operation: '%'));
+}
